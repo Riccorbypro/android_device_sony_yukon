ba55b92424f8367647f33f16cb625b702f7dc4bc
Riccorbypro <riccorbypro@gmail.com>
2016-01-04 18:35:01 +0200

    Corrected CPU variant

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ BOARD_CUSTOM_MKBOOTIMG := mkqcdtbootimg
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --dt_dir $(OUT)/dtbs --dt_version 2
 
-BOARD_KERNEL_CMDLINE := androidboot.hardware=yukon androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37
 BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8
 BOARD_KERNEL_CMDLINE += coherent_pool=8M vmalloc=400M
